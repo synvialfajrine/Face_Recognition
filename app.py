97e863ec76753369ebdeaaecda34b468df91bfc3
Synvi Alfajrine <76579802+synvialfajrine@users.noreply.github.com>
2024-02-29 11:12:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 #INITIALIZE
 facenet = FaceNet()
-faces_embeddings = np.load("faces_embeddings.npz")
+faces_embeddings = np.load("https://github.com/synvialfajrine/Face_Recognition/blob/main/faces_embeddings.npz")
 Y = faces_embeddings['arr_1']
 encoder = LabelEncoder()
 encoder.fit(Y)
-haarcascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-model = pickle.load(open("svm_model_160x160.pkl", 'rb'))
+haarcascade = cv2.CascadeClassifier("https://github.com/synvialfajrine/Face_Recognition/blob/main/haarcascade_frontalface_default.xml")
+model = pickle.load(open("https://github.com/synvialfajrine/Face_Recognition/blob/main/svm_model_160x160.pkl", 'rb'))
 
 # Content for the sidebar
 sidebar_content = """
